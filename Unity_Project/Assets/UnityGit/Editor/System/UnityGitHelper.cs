f779528e8da52c4387d4610f5ca24a899cf871e6
NateWardawg <nathanwardenlee@mac.com>
2011-06-29 20:58:49 -0500

    Renamed Git to UnityGit

---
+++ Unity_Project/Assets/UnityGit/Editor/System/UnityGitHelper.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEditor;
+
+using System.Collections;
+
+public class UnityGitHelper : MonoBehaviour {
+	public static void CleanupUntracked()
+	{
+		Debug.Log(GitSystem.RunGitCmd("clean -d -f"));
+
+		UnityGitHelper.CleanupUntracked(GitSystem.GetUntrackedFilesList(false));
+	}
+
+
+	static void CleanupUntracked(string[] untrackedFiles)
+	{
+		foreach ( string path in untrackedFiles)
+		{
+			Debug.Log(path);
+			AssetDatabase.DeleteAsset(path);
+		}
+
+		AssetDatabase.Refresh();
+	}
+}
